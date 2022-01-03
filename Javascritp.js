cb5f6209bfebe51e5e653eacef619e7e1d5c099e
Léo Ferron <93705260+ZapBzh@users.noreply.github.com>
2022-01-03 10:35:16 +0100

    Add files via upload

---
+++ Javascritp.js
@@ -0,0 +1,10 @@
+//Définition des variable//
+
+var PV = document.getElementById("PV");
+var Mana = document.getElementById("Mana");
+
+Mana.onclick = function() {
+    if()
+    Mana.innerHTML =  Mana.innerHTML - 25;
+
+}
